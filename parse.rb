27689394fa1b7ef6aa95a8bb0ac895af1fa23c4a
Alexander Ekdahl <alexander@ekdahl.io>
2013-09-06 12:47:11 +0200

    Improved parsing - validates using google books api

--- parse.rb
+++ parse.rb
@@ -2,6 +2,10 @@
 # TODO Use another service when google books api fails
 #  - http://docs.aws.amazon.com/AWSECommerceService/latest/DG/ItemLookup.html
 # TODO Parse program/year
+# TODO parse lu http://www.lu.se/lubas/courses
+
+
+# CSV structure - ISBN13, Title, Authors, Courses
 
 require 'nokogiri'
 require 'open-uri/cached'
@@ -14,23 +18,61 @@ def strip_isbn(isbn)
 end
 
 def scan(page)
-  page.scan(/ISBN:?(?: 1[30])?:?\s([0-9x -]*)/i).flatten.map { |isbn| strip_isbn(isbn) }
+  page.scan(/ISBN:?(?:\s?1[30])?:?\s?([0-9][0-9Xx -]*)/).flatten.map { |isbn| strip_isbn(isbn) }
+end
+
+def find_isbn13(book)
+  return nil unless book['volumeInfo']['industryIdentifiers']
+
+  book['volumeInfo']['industryIdentifiers'].each do |id|
+    return id['identifier'] if id['type'] == 'ISBN_13'
+  end
+  nil
+end
+
+def find_authors(book)
+  book['volumeInfo']['authors']
+end
+
+def find_title(book)
+  book['volumeInfo']['title']
+end
+
+def google_books_api(isbn)
+  books = JSON.parse(open("https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}&key=AIzaSyDdnBhErY3yMQ2UB6OBz6AIRlp6vrTlYsM").read)
+  books['items'] ? books['items'].first : nil
 end
 
 if __FILE__ == $0
   courses = Nokogiri::HTML(open("http://kurser.lth.se/kursplaner/12_13/"))
   table   = courses.css('.courselist table')
   rows    = table.css('tr')
+  books_and_codes = Hash.new { |hash, key| hash[key] = [] }
+  books   = {}
+
+  rows.each do |row|
+    code, link, _, name = row.css('td')
+    page  = open("http://kurser.lth.se#{link.child[:href]}").read
+    isbns = scan(page)
+
+    isbns.each do |isbn|
+      books_and_codes[isbn] << "#{code.text} - #{name.text}"
+    end
+  end
+
+  books_and_codes.each do |key, value|
+    book = google_books_api(key)
+
+    if book and (id = find_isbn13(book))
+      if id
+        books[id] = [find_title(book), find_authors(book), value]
+      end
+    end
+  end
 
   CSV.open("lth.csv", "wb") do |csv|
-    rows.each do |row|
-      code, link, _, name = row.css('td')
-      page  = open("http://kurser.lth.se#{link.child[:href]}").read
-      isbns = scan(page)
-
-      isbns.each do |isbn|
-        csv << [code.text, name.text, isbn]
-      end
+    books.each do |key, values|
+      csv << [key, *values]
     end
   end
 end
--- parse_test.rb
+++ parse_test.rb
@@ -3,13 +3,27 @@ require './parse'
 
 class TestExamples < Test::Unit::TestCase
   def test_scan
-    assert_scan("0867589094", "ISBN 0-86758-909-4")
-    assert_scan("9789144066257", "ISBN: 978-91-44-06625-7")
-    assert_scan("9780415419468", "ISBN: 9780415419468")
-    assert_scan("9780415419468", "ISBN 13: 9780415419468")
-    assert_scan("012375030", "ISBN: 10: 012375030")
-    assert_scan("9780123750303", "ISBN: 13: 9780123750303")
-    assert_scan("047124824X", "ISBN 0-471-24824-X")
+    assert_scan('0867589094',    'ISBN 0-86758-909-4')
+    assert_scan('9789144066257', 'ISBN: 978-91-44-06625-7')
+    assert_scan('9780415419468', 'ISBN: 9780415419468')
+    assert_scan('9780415419468', 'ISBN 13: 9780415419468')
+    assert_scan('012375030',     'ISBN: 10: 012375030')
+    assert_scan('9780123750303', 'ISBN: 13: 9780123750303')
+    assert_scan('047124824X',    'ISBN 0-471-24824-X')
+    assert_scan('9780465025787', 'ISBN: 13:9780465025787')
+    assert_scan('0415357977',    'ISBN10: 0-415-35797-7')
+    assert_equal([],        scan('ISBN: Diehl, S. Kapitel P,T, A.'))
+  end
+
+  def test_find_isbn13
+    book = google_books_api('0471209066')
+    assert_equal('9780471209065', find_isbn13(book))
+    assert_equal(["Frank Ching"], find_authors(book))
+
+    book = google_books_api('9789144066257')
+    assert_equal('9789144066257', find_isbn13(book))
+    assert_equal("Flervariabelanalys med Maple", find_title(book))
+    assert_equal(["Gerd Brandell", "Anders Holst", "Sigrid Sjöstrand"], find_authors(book))
   end
 
   def assert_scan(result, page)
